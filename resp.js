516f0b3452420a4f683752f578476fe4703bc1cb
Satis Kumar Chaudhary <66665573+Satis9815@users.noreply.github.com>
2020-10-12 05:18:40 -0700

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,11 @@
+console.log('This is the tutorial for responsive website');
+burger=document.querySelector('.burger');
+navBar=document.querySelector('.navbar');
+navList=document.querySelector('.nav-list');
+rightNav=document.querySelector('.rightnav');
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class');
+    navList.classList.toggle('v-class');
+    navBar.classList.toggle('h-nav');
+})
